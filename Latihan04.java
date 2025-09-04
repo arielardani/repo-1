e0b0c96361cf2c8edbd5519dc75eb1e0be9dc2c9
Ariel Ardani <arielardani20@gmail.com>
2025-09-04 21:18:24 +0700

    Add files via upload

---
+++ Latihan04.java
@@ -0,0 +1,7 @@
+public class Latihan04 {
+    public static void main(String[] args) {
+         System.out.println("Halo, nama saya adalah Ariel");
+         System.out.print ("\nBelajar Java itu mudah!");
+//         System.out.print("\nSemakin sering mencoba, semakin jago");
+    }
+}
---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+public static void main(String[] args) {
+    float berat = 0.5467812345F;
+    System.out.println("Berat\t\t\t\t\t\t\t: " + (double) berat);
+}
+}
